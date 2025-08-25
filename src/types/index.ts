f82c30c41c46c8dd40425ac2dbede082fd5c498a
richard.schmidt <richard.schmidt@htri.net>
2025-08-25 09:17:48 -0500

    add interfaces for post request

--- src/types/index.ts
+++ src/types/index.ts
@@ -20,3 +20,27 @@ export interface BusinessPartner {
   City?: string;
   Country?: string;
 }
+
+export interface OutlookActivity {
+  CardCode: string;
+  Subject: string;
+  Body: string;
+  ProjectCode: string;
+  ShouldCreateFollowUp: boolean;
+  ShouldCreateReminder: boolean;
+  ShouldSaveMessage: boolean;
+  ShouldSaveAttachments: boolean;
+  ActivityTypeId: number;
+  ActivityTypeName?: string; // Optional since it's not always needed
+  FollowUpStartDate: string;
+  FollowUpEndDate: string;
+  ReminderDate: string;
+  ReminderTime: number; // Time as HHMM (like 1605 for 4:05 PM)
+  ReminderType: string; // "M" for Minutes, "H" for Hours, "D" for Days
+  ReminderQuantity: number;
+}
+
+export interface CreateActivityResponse {
+  didSave: boolean;
+  didFollowUpSave: boolean;
+}
